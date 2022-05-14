6fda1216115522f0417f578cd5f8e217d619ca5b
tianfengyou <420144542@qq.com>
2022-05-15 07:25:17 +0800

    xiugai
    xiugai

--- TFYSwiftNavigationController/Class/BarHiddenViewController.swift
+++ TFYSwiftNavigationController/Class/BarHiddenViewController.swift
@@ -11,21 +11,13 @@ class BarHiddenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tfy_barAlpha = 0
-        tfy_tintColor = .clear
-        tfy_titleColor = .clear
-        tfy_barStyle = .default
+//        tfy_barAlpha = 0
+//        tfy_tintColor = .clear
+//        tfy_titleColor = .clear
+        tfy_barStyle = .black
+        tfy_backgroundColor = .orange
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
+
     
     @IBAction func back() {
         navigationController?.popViewController(animated: true)
